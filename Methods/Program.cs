ea6e535cedb43ce25100ab39a25547b752a1cb65
Kamil Grabiec <grabiec.kamil@gmail.com>
2020-04-02 18:46:15 +0200

    method

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -22,12 +22,44 @@ namespace Methods
             divide.DivideBy(6, 3);
             */
 
+
+            /*
             NamedParameters multiple = new NamedParameters();
             //Default values
             Console.WriteLine(multiple.VolumeCalculator());
 
             //Volume of figure where length = 6 and width
             Console.WriteLine(multiple.VolumeCalculator(w:4, l:6));
+            */
+
+            
+            PassingArguments passingArguments = new PassingArguments();
+
+            //Passing argument by value
+            int a = 3;
+            passingArguments.ByValue(a);
+            Console.WriteLine(a);
+
+            //Passing argument as reference
+            /*
+             * Pass by reference copies an argument's memory address into the formal parameter. Inside the method, the address is used to access the actual argument used in the call. This means that changes made to the parameter affect the argument.
+              * To pass the value by reference, the ref keyword is used in both the call and the method definition:
+             */
+
+            int b = 3;
+            //The ref keyword is used both when defining the method and when calling it.
+            passingArguments.ByReference(ref b);
+            Console.WriteLine(b);
+
+            int c;
+            passingArguments.ByOutput(out c);
+            Console.WriteLine(c);
+
+            TemperatureCalculator temperatureCalculator = new TemperatureCalculator();
+            temperatureCalculator.CalculateTemperature();
+
+
+
 
             Console.ReadKey();
         }
---
+++ Methods/TemperatureCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class TemperatureCalculator
+    {
+        static void Celcius(double t, out double kelvin, out double fahrenheit)
+        {
+            fahrenheit = t*9/5+32;
+            // why t*(9/5)+32 does not work?!
+            kelvin = t+273.15;
+        }
+
+        static void Kelvin(double t, out double celcius, out double fahrenheit)
+        {
+            celcius = t-273.15;
+            fahrenheit = ((t-273.15)*(9/5))+32;
+        }
+
+        static void Fahrenheit(double t, out double celcius, out double kelvin)
+        {
+            celcius = (t-32)*(5/9);
+            kelvin = (t - 32) * (5 / 9)+273.15;
+        }
+
+        public void CalculateTemperature()
+        {
+            Console.WriteLine("Type 1 to convert Celcius, 2 to convert Kelvins and 3 to convert Fahrenheids");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.Write("Temperature in Celcius is:");
+                    double t1 = Convert.ToDouble(Console.ReadLine());
+                    double kelvin, fahrenheit;
+                    Celcius(t1, out kelvin, out fahrenheit);
+                    Console.WriteLine("Kelvins {0}, Fahrenheits {1}", kelvin, fahrenheit);
+
+                    break;
+                case 2:
+                    Console.Write("Temperature in Kelvins is:");
+                    double t2 = Convert.ToDouble(Console.ReadLine());
+                    break;
+
+                case 3:
+                    Console.Write("Temperature in Fahrenheids is:");
+                    double t3 = Convert.ToDouble(Console.ReadLine());
+                    break;
+            }
+
+
+        }
+
+    }
+}
