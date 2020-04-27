28baf3bae7aa4ac658b957eeca1ab0a057758d38
Kamil Grabiec <grabiec.kamil@gmail.com>
2020-04-27 12:04:21 +0200

    ClassesAndObjecs finish

--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -16,15 +16,31 @@ namespace ClassesAndObjects
             gosiaAccount.NextAction();
             */
 
+            /*
             Person p = new Person("Gosia");
             Console.WriteLine(p.GetName());
+            */
 
+            /* 
             WHCharacterCreator gosia = new WHCharacterCreator("krasnolud");
             gosia.GetStats(out int returnHp, out int returMn, out int returnLvl, out string returnRace);
             Console.WriteLine(Convert.ToString(returnHp) + " " + Convert.ToString(returMn) + " " + Convert.ToString(returnLvl) + " " + returnRace);
+            
+            */
+
+            Properties_Pet dog1 = new Properties_Pet("Dog", "Golden Retriver");
+            dog1.Name = "Blant";
+            dog1.Age = 1;
+
+            Console.WriteLine("{0}{1}{2}{3}", dog1.Species, dog1.Race, dog1.Name, dog1.Age);
+
+
+
+
+
+
             Console.ReadKey();
 
-  
         }
     }
 }
