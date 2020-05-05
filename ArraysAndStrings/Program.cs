f9ecea1c1420af8586d315926a58fc054dc66cc7
Kamil Grabiec <grabiec.kamil@gmail.com>
2020-05-05 09:26:10 +0200

    First Arrays commit

---
+++ ArraysAndStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicArrays p = new BasicArrays();
+            p.FirstArray();
+            p.PreDefinedArray();
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
